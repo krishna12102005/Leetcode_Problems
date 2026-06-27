5e5cd7fb7de36023bb34491adfadf6c91f0436e2
Krishna K <krishnakalaiselvan12@gmail.com>
2026-06-27 09:17:41 +0530

    Time: 32 ms (56.77%), Space: 46.4 MB (45.66%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.java
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.java
@@ -1,54 +1,33 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
-        int count = 0;
+        int count= 0;
         for(int i = low ; i <= high ; i++){
-                int[] arr =  ConvertArray(i);
+            String str = String.valueOf(i);
 
-                if(arr.length % 2 == 0){
-                    int mid = arr.length / 2;
-                    int[] left = Arrays.copyOfRange(arr , 0 , mid);
-                    int[] right = Arrays.copyOfRange(arr , mid , arr.length);
 
-                    if(sumOfArray(left , right)){
-                           count++;
-                    }
+            if(str.length() % 2 == 0){
+                int first = 0;
+                int second = 0;
 
+                int mid = str.length() / 2;
+
+                for(int j = 0 ; j < mid ; j++){
+                    first = first + str.charAt(j) - '0';
                 }
-                
 
-            
+                for(int k = mid ; k < str.length() ; k++){
+                    second = second + str.charAt(k) - '0';
+                }
 
-        }
 
-        return count ;
-        
-    }
-    public static boolean sumOfArray(int[] arr1 , int[] arr2){
-        int sum1 = 0;
-        int sum2 = 0;
+                if(first == second){
+                    count++;
+                }
 
-        for(int i = 0 ; i < arr1.length ; i++){
-            sum1 = sum1 + arr1[i];
-        }
-
-        for(int i = 0 ; i < arr2.length  ; i++){
-            sum2 = sum2 + arr2[i];
+            }
         }
 
 
-        return sum1 == sum2;
-    }
-
-    public static int[]  ConvertArray(int n){
-
-        String str = Integer.toString(n);
-        int[] arr = new int[str.length()];
-
-        for (int i = 0; i < str.length(); i++) {
-        arr[i] = str.charAt(i) - '0';
-
-        }
-
-        return arr;
+        return count;
     }
 }
