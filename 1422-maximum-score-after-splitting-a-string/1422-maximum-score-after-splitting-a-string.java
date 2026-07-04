4312c50294098c20855d91cd2ff02b5ec84cae25
Krishna K <krishnakalaiselvan12@gmail.com>
2026-07-04 18:22:32 +0530

    Time: 1 ms (99.52%), Space: 42.4 MB (98.64%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,38 +1,28 @@
 class Solution {
     public int maxScore(String s) {
         int max = 0;
-        for(int i = 1 ;i < s.length() ; i++){
-            int first = left(s.substring(0,i));
-            int second = right(s.substring(i));
-
-            int sum = first + second;
-            max = Math.max(sum , max);
+        int totalOnes = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '1'){
+                totalOnes++;
+            }
         }
 
+        int leftzeros = 0;
+
+        for(int i = 0 ; i < s.length()-1 ; i++){
+            if(s.charAt(i) == '0'){
+                leftzeros++;
+            }
+            else{
+                totalOnes--;
+            }
+
+            max = Math.max(max , leftzeros + totalOnes);
+            
+        }
 
         return max;
-        
-    }
-    public int left(String s){
-        int count = 0;
-        for(int i = 0 ; i < s.length() ; i++){
-            if(s.charAt(i) == '0'){
-                count++;
-            }
 
-        }
-
-        return count;
-    }
-
-    public int right(String s){
-        int count = 0;
-        for(int i = 0 ; i< s.length() ; i++){
-            if(s.charAt(i) == '1'){
-                count++;
-            }
-        }
-
-        return count;
     }
 }
