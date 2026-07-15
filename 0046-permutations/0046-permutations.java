91d9d30f6829c7e6d9f9d67e576c6038fe37222c
Krishna K <krishnakalaiselvan12@gmail.com>
2026-07-15 15:21:23 +0530

    Time: 1 ms (87.2%), Space: 45.5 MB (63.64%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        PermutationBuilding(nums , ds , freq , ans);
+
+        return ans;
+        
+    }
+    public void PermutationBuilding(int[] nums , List<Integer> ds , boolean[] freq , List<List<Integer>> ans ){
+        if(ds.size() == nums.length){
+            ans.add(new ArrayList<>(ds));
+            return ;
+        }
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(!freq[i]){
+                freq[i] = true;
+                ds.add(nums[i]);
+                PermutationBuilding(nums , ds , freq , ans);
+                ds.remove(ds.size() - 1);
+                freq[i] = false;
+            }
+        }
+
+    }
+}
